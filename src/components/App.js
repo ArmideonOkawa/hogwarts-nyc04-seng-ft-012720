6a71836e978989f4a936e6de9246e71debc24222
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-04-02 17:03:35 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,73 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import PigIndex from './PigIndex';
+// import Filter from './Filter';
+
+const duplicateHogs = [...hogs]
 
 class App extends Component {
+
+  state = {
+    allPigs: hogs
+    //isGreased
+    //isHidden
+  }
+
+  nameWeightFilter = (value) => {
+    if (value === "name") {
+      this.setState({
+        allPigs: hogs.sort(function(a, b) {
+          var nameA = a.name.toUpperCase();
+          var nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+          return -1;
+          }
+          if (nameA > nameB) {
+          return 1;
+          }
+          return 0;
+        })
+      })
+    } else if (value === "weight") {
+      this.setState({
+        allPigs: hogs.sort(function (a, b) {
+          var weightA = a.weight
+          var weightB = b.weight
+          return weightA - weightB;
+          
+        })
+      })
+    } else {
+      this.setState({
+        allPigs: duplicateHogs
+      })
+    }
+  }
+
+  greaseFilter = (value) => {
+
+    if(value === "greased"){
+      this.setState({
+        allPigs: hogs.filter(pig => pig.greased === true)
+      })
+    }else if (value === "not-greasy"){
+      this.setState({
+        allPigs: hogs.filter(pig => pig.greased === false)
+      })
+    }else{
+      this.setState({
+        allPigs: duplicateHogs
+      })
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Nav />
-        <HelloWorld />
+        <Nav nameWeightFilter={this.nameWeightFilter} greaseFilter={this.greaseFilter} />
+        <PigIndex allPigs={this.state.allPigs}/>
       </div>
+    
     );
   }
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,48 @@
-import piggy from "../porco.png";
-import React from "react";
+import piggy from '../porco.png'
+import React from 'react'
 
-const Nav = () => {
-  return (
-    <div className="navWrapper">
-      <span className="headerText">Hogwarts</span>
-      <div className="TwirlyPig">
-        <img src={piggy} className="App-logo" alt="piggy" />
-      </div>
-      <span className="normalText">A React App for County Fair Hog Fans</span>
-    </div>
-  );
-};
 
-export default Nav;
+const Nav = (props) => {
+
+	const handleRadio = (evt) => {
+		props.greaseFilter(evt.target.value)
+	}
+
+	const handleDropdown = (evt) => {
+		props.nameWeightFilter(evt.target.value)
+	}
+
+	return (
+	  <div className="navWrapper">
+		<span className="headerText">Hogwarts</span>
+		<div className="TwirlyPig">
+		  <img src={piggy} className="App-logo" alt="piggy" />
+		</div>
+		<span className="normalText">A React App for County Fair Hog Fans</span>
+	<div>
+		<div onChange={handleDropdown}>
+		 <label>Filter By:</label>
+		 <select>
+		  <option value="no-filter"></option>
+		  <option value="name">Name</option>
+		  <option value="weight">Weight</option>
+		 </select>
+		</div>
+		<div onChange={handleRadio}>
+		 <input type="radio" name="greasy" value="all"/> 
+		 <label>All Hogs</label>
+		 
+		 <input type="radio" name="greasy" value="greased"/>
+		 <label>Greased Hogs</label>
+
+		 <input type="radio" name="greasy" value="not-greasy"/>
+		 <label>Ungreased Hogs</label>
+		</div>
+
+	</div>
+	</div>
+	  
+	);
+  };
+  
+  export default Nav;
